56371355b4b8c72d950b7c6016d792a9a5506ffb
Christopher Galpin <52485+CodeOptimist@users.noreply.github.com>
2021-02-10 16:19:45 -0700

    README & example code

--- ahkunwrapped/__init__.py
+++ ahkunwrapped/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '2.0.0'
+__version__ = '2.0.0.post1'
 from .autohotkey import *
--- example.py
+++ example.py
@@ -1,36 +1,68 @@
+import sys
 import time
 from datetime import datetime
-from autohotkey import Script
+from enum import Enum
+from pathlib import Path
+
+from ahkunwrapped import Script, AhkExitException
+
+choice = None
+HOTKEY_SEND_CHOICE = 'F2'
 
 
-ahk = Script("""
-Send(text) {
-    Send, % text
-}
-
-AutoExec() {
-    SetTitleMatchMode, 2
-    MsgBox, Initialized.
-}
-
-#IfWinActive, Notepad
-CapsLock::hotkey = insert time
-""")
+class Event(Enum):
+    QUIT, SEND_CHOICE, CLEAR_CHOICE, CHOOSE_MONTH, CHOOSE_DAY = range(5)
 
 
-def poll():
-    hotkey = ahk.get('hotkey')
-    if not hotkey:
-        return
-    ahk.set('hotkey', '')
-    if hotkey == "insert time":
-        ahk.call('Send', datetime.now().strftime('%#I:%M %p'))
+ahk = Script.from_file(Path('example.ahk'), format_dict=globals())
+
+
+def main() -> None:
+    ts = 0
+    while True:
+        exit_code = ahk.poll()
+        if exit_code:
+            sys.exit(exit_code)
+
+        try:
+            s_elapsed = time.time() - ts
+            if s_elapsed >= 60:
+                ts = time.time()
+                print_minute()
+
+            event = ahk.get('event')
+            if event:
+                ahk.set('event', '')
+                on_event(event)
+        except AhkExitException:
+            print("Graceful exit.")
+            sys.exit(0)
+        time.sleep(0.01)
+
+
+def print_minute() -> None:
+    print(f'It is now {datetime.now().time()}')
+
+
+def on_event(event: str) -> None:
+    global choice
+
+    def get_choice() -> str:
+        return choice or datetime.now().strftime('%#I:%M %p')
+
+    if event == str(Event.QUIT):
+        ahk.exit()
+    if event == str(Event.CLEAR_CHOICE):
+        choice = None
+    if event == str(Event.SEND_CHOICE):
+        ahk.call('Send', f'{get_choice()} ')
+    if event == str(Event.CHOOSE_MONTH):
+        choice = datetime.now().strftime('%b')
+        ahk.call('ToolTip', f'Month is {get_choice()}, {HOTKEY_SEND_CHOICE} to insert.')
+    if event == str(Event.CHOOSE_DAY):
+        choice = datetime.now().strftime('%#d')
+        ahk.call('ToolTip', f'Day is {get_choice()}, {HOTKEY_SEND_CHOICE} to insert.')
 
 
 if __name__ == '__main__':
-    # ahk2 = Script.from_file(r'example.ahk')
-    # ahk2.call('MsgBox', "Hello World!")
-
-    while True:
-        poll()
-        time.sleep(0.01)
+    main()
